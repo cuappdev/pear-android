a7567195f55969a47ce8d089aff2529c736e0dbb
Pdbz199 <279pdbz199@gmail.com>
2020-02-16 01:34:53 -0500

    Mostly finished Profile Creation page

--- app/src/main/java/com/example/coffee_chats_android/ClubInterestAdapter.kt
+++ app/src/main/java/com/example/coffee_chats_android/ClubInterestAdapter.kt
@@ -1,15 +1,18 @@
 package com.example.coffee_chats_android
 
 import android.content.Context
+import android.graphics.PorterDuff
+import android.support.constraint.ConstraintLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import com.example.coffee_chats_android.Models.ClubOrInterest
 
 
-class ClubInterestAdapter(private val mContext: Context, list: Array<String>) :
-    ArrayAdapter<String?>(mContext, 0, list) {
+class ClubInterestAdapter(private val mContext: Context, list: Array<ClubOrInterest>) :
+    ArrayAdapter<ClubOrInterest?>(mContext, 0, list) {
     private var clubInterestList = list
     override fun getView(
         position: Int,
@@ -21,16 +24,17 @@ class ClubInterestAdapter(private val mContext: Context, list: Array<String>) :
             LayoutInflater.from(mContext).inflate(R.layout.club_or_interest_view, parent, false)
         val currentClubInterest = clubInterestList[position]
         val clubOrInterestText = listItem!!.findViewById<TextView>(R.id.club_or_interest_text)
-        clubOrInterestText.setText(currentClubInterest)
-//        val image =
-//            listItem!!.findViewById<View>(R.id.imageView_poster) as ImageView
-//        image.setImageResource(currentMovie.getmImageDrawable())
-//        val name =
-//            listItem.findViewById<View>(R.id.textView_name) as TextView
-//        name.setText(currentMovie.getmName())
-//        val release =
-//            listItem.findViewById<View>(R.id.textView_release) as TextView
-//        release.setText(currentMovie.getmRelease())
+        clubOrInterestText.setText(currentClubInterest.getText())
+        val clubOrInterestSubtext = listItem!!.findViewById<TextView>(R.id.club_or_interest_subtext)
+        clubOrInterestSubtext.setText(currentClubInterest.getSubtext())
+
+        val selected = context.resources.getColor(R.color.selected_interest_or_club)
+        val unselected = context.resources.getColor(R.color.unselected_interest_or_club)
+        val layout = listItem!!.findViewById<ConstraintLayout>(R.id.club_or_interest_box)
+        val drawableBox = layout.background
+        if (currentClubInterest.isSelected()) drawableBox.setColorFilter(selected, PorterDuff.Mode.MULTIPLY)
+        else drawableBox.setColorFilter(unselected, PorterDuff.Mode.MULTIPLY)
+
         return listItem
     }
 
--- app/src/main/java/com/example/coffee_chats_android/MainActivity.kt
+++ app/src/main/java/com/example/coffee_chats_android/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.coffee_chats_android
 
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
+import com.example.coffee_chats_android.Models.ClubOrInterest
 import kotlinx.android.synthetic.main.fragment_create_profile.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +19,26 @@ class MainActivity : AppCompatActivity() {
     lateinit var adapter: ClubInterestAdapter
     lateinit var nextButton: Button
     lateinit var backButton: Button
-    val interests = arrayOf("Art", "Business", "Design", "Humanities", "Fitness & Sports", "Tech", "More")
-    val clubs = arrayOf("AppDev", "DTI", "Guac Magazine", "GCC", "CVC", "CVS")
+    val interestTitles = arrayOf("Art", "Business", "Design", "Humanities", "Fitness & Sports", "Tech", "More")
+    val interestSubtitles =  arrayOf("painting crafts, embroidery", "finance, entrepreneurship, VC", "UX/UI, graphic, print",
+                                        "history, politics", "working out, outdoors, basketball", "random technology", "there is more")
+    val clubTitles = arrayOf("AppDev", "DTI", "Guac Magazine", "GCC", "CVC", "CVS")
+
     lateinit var interestsAndClubs: ListView
+    var interests : Array<ClubOrInterest> = Array(interestTitles.size) { index -> ClubOrInterest("", "") }
+    var clubs : Array<ClubOrInterest> = Array(clubTitles.size) { index -> ClubOrInterest("", "") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_create_profile)
+
+        for (i in 0 until interestTitles.size) {
+            interests[i] = ClubOrInterest(interestTitles[i], interestSubtitles[i])
+        }
+
+        for (i in 0 until clubTitles.size) {
+            clubs[i] = ClubOrInterest(clubTitles[i], "")
+        }
 
         header = findViewById(R.id.signup_header)
         nextButton = findViewById(R.id.signup_next)
@@ -31,6 +49,17 @@ class MainActivity : AppCompatActivity() {
 
         adapter = ClubInterestAdapter(this, interests)
         interestsAndClubs = findViewById(R.id.interests_or_clubs)
+        interestsAndClubs.adapter = adapter
+
+        val selected = resources.getColor(R.color.selected_interest_or_club)
+        val unselected = resources.getColor(R.color.unselected_interest_or_club)
+        interestsAndClubs.setOnItemClickListener { parent, view, position, id ->
+            val selectedView = view.findViewById<ConstraintLayout>(R.id.club_or_interest_box)
+            val drawableBox = selectedView.background
+            interests[position].toggleSelected()
+            if (interests[position].isSelected()) drawableBox.setColorFilter(selected, PorterDuff.Mode.MULTIPLY)
+            else drawableBox.setColorFilter(unselected, PorterDuff.Mode.MULTIPLY)
+        }
 
         updatePage()
     }
