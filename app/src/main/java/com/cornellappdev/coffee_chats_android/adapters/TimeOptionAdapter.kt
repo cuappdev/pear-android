d24bbb39507303b8af15f7450ff22fda82a21eae
Haichen Wang <hw595@cornell.edu>
2020-05-19 19:17:21 +0800

    Removed unused TimeAdapter, used ViewHolder for TimeOptionAdapter, fixed nits

--- app/src/main/java/com/cornellappdev/coffee_chats_android/adapters/TimeOptionAdapter.kt
+++ app/src/main/java/com/cornellappdev/coffee_chats_android/adapters/TimeOptionAdapter.kt
@@ -17,14 +17,25 @@ class TimeOptionAdapter(private val mContext: Context, list: Array<String>) :
         convertView: View?,
         parent: ViewGroup
     ): View {
-        val listItem = LayoutInflater.from(mContext).inflate(R.layout.time_option_item, parent, false)
-        val timeTextView = listItem.findViewById<TextView>(R.id.time_option_text)
-        timeTextView.text = list[position]
-
+        val viewHolder: ViewHolder
+        val listItem: View
+        if (convertView == null) {
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.time_option_item, parent, false)
+            viewHolder = ViewHolder(listItem)
+            listItem.tag = viewHolder
+        } else {
+            listItem = convertView
+            viewHolder = listItem.tag as ViewHolder
+        }
+        viewHolder.timeTextView!!.text = list[position]
         return listItem
     }
 
     init {
         this.list = list
+    }
+
+    private class ViewHolder(view: View?) {
+        val timeTextView = view?.findViewById<TextView>(R.id.time_option_text)
     }
 }
