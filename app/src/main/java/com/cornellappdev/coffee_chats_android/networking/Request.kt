190029d367da55357a942a3de71eba2a8ceda0ed
Shiyuan-Huang-23 <shiyuancn@live.cn>
2021-02-21 11:38:29 -0500

    Set up infrastructure for backend integration

---
+++ app/src/main/java/com/cornellappdev/coffee_chats_android/networking/Request.kt
@@ -0,0 +1,61 @@
+package com.cornellappdev.coffee_chats_android.networking
+
+import com.google.gson.Gson
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import java.io.IOException
+import java.lang.reflect.Type
+
+object Request {
+    val httpClient = OkHttpClient()
+
+    suspend inline fun <reified T> makeCall(request: okhttp3.Request, typeToken: Type): T? {
+        val response = httpClient.newCall(request).await()
+        val responseBody = response.body
+        val responseBodyString = responseBody?.string() ?: ""
+
+        val responseBodyJSON = Gson()
+
+        return responseBodyJSON.fromJson<T>(responseBodyString, typeToken)
+    }
+
+    /**
+     * Suspend extension that allows suspend [Call] inside coroutine.
+     *
+     * @return Result of request or throw exception
+     */
+    suspend fun Call.await(recordStackTrace: Boolean = true): Response {
+        val stackTrace =
+            if (recordStackTrace) IOException("Exception occurred while awaiting Call.") else null
+        return suspendCancellableCoroutine { continuation ->
+            enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    continuation.resume(response)
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    // Don't bother with resuming the continuation if it is already cancelled.
+                    if (stackTrace != null) {
+                        stackTrace.initCause(e)
+                        continuation.resumeWithException(stackTrace)
+                    } else {
+                        continuation.resumeWithException(e)
+                    }
+                }
+            })
+
+            continuation.invokeOnCancellation {
+                try {
+                    cancel()
+                } catch (ex: Throwable) {
+                    //Ignore cancel exception
+                }
+            }
+        }
+    }
+}
