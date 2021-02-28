d50efcd0833a209c533fb09b0a79215b0c3ae777
Shiyuan-Huang-23 <shiyuancn@live.cn>
2021-02-28 13:47:58 -0500

    Integrate CreateProfileActivity setup with backend

--- app/src/main/java/com/cornellappdev/coffee_chats_android/CreateProfileActivity.kt
+++ app/src/main/java/com/cornellappdev/coffee_chats_android/CreateProfileActivity.kt
@@ -6,84 +6,111 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.cornellappdev.coffee_chats_android.models.InternalStorage
-import com.cornellappdev.coffee_chats_android.models.UserProfile
+import com.cornellappdev.coffee_chats_android.models.ApiResponse
+import com.cornellappdev.coffee_chats_android.models.User
+import com.cornellappdev.coffee_chats_android.networking.Endpoint
+import com.cornellappdev.coffee_chats_android.networking.Request
+import com.cornellappdev.coffee_chats_android.networking.getAllMajors
+import com.cornellappdev.coffee_chats_android.networking.getUser
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_create_profile.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 class CreateProfileActivity : AppCompatActivity() {
 
+    // variables to keep track if editTexts are filled out
+    private var majorFilled = false
+    private var hometownFilled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
 
-        // variables to keep track if editTexts are filled out
-        var majorFilled = false
-        var hometownFilled = false
+        CoroutineScope(Dispatchers.Main).launch {
+            val getUserEndpoint = Endpoint.getUser()
+            val userTypeToken = object : TypeToken<ApiResponse<User>>() {}.type
+            val user = withContext(Dispatchers.IO) {
+                Request.makeRequest<ApiResponse<User>>(
+                    getUserEndpoint.okHttpRequest(),
+                    userTypeToken
+                )
+            }!!.data
+            demographicsHeader.text = getString(R.string.demographics_header, user.firstName)
+            // pre-fills existing user profile information
+            if (!user.hometown.isNullOrBlank()) {
+                hometownEditText.setText(user.hometown)
+                hometownFilled = true
+            }
+            if (!user.major.isNullOrBlank()) {
+                majorACTV.setText(user.major)
+                majorFilled = true
+            }
+            // nextButton is disabled until user has filled out all required info
+            if (user.hometown.isNullOrBlank() || user.major.isNullOrBlank()) {
+                nextButton.isEnabled = false
+                nextButton.isClickable = false
+            }
 
-        // tries to retrieve User Profile from internal storage
-        val profile = InternalStorage.readObject(this, "profile") as UserProfile
-        demographicsHeader.text = getString(R.string.demographics_header, profile.userName)
-        if (profile.hometown.isNotEmpty())  {
-            hometownEditText.setText(profile.hometown)
-            hometownFilled = true
-        }
-        if (profile.major.isNotEmpty()) {
-            majorACTV.setText(profile.major)
-            majorFilled = true
-        }
+            // Initializing the class spinner
+            val classArray = ArrayList<String>()
+            var year = Calendar.getInstance().get(Calendar.YEAR)
+            val month = Calendar.getInstance().get(Calendar.MONTH)
+            if (month >= 6) year++  // sets minimum graduation year to next year
+            for (i in 0..4) {
+                classArray.add("Class of " + (year + i))
+            }
+            val classArrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+                applicationContext,
+                R.layout.profile_spinner_item,
+                classArray
+            )
+            classSpinner.adapter = classArrayAdapter
+            if (!user.graduationYear.isNullOrBlank()) classSpinner.setSelection(
+                Integer.parseInt(
+                    user.graduationYear
+                ) - year
+            )
 
-        // nextButton is disabled until user has filled out all required info
-        if (!hometownFilled || !majorFilled) {
-            nextButton.isEnabled = false
-            nextButton.isClickable = false
-        }
+            // Initializing the major AutoCompleteTextView
+            val getMajorsEndpoint = Endpoint.getAllMajors()
+            val majorsTypeToken = object : TypeToken<ApiResponse<List<String>>>() {}.type
+            val majors = withContext(Dispatchers.IO) {
+                Request.makeRequest<ApiResponse<List<String>>>(
+                    getMajorsEndpoint.okHttpRequest(),
+                    majorsTypeToken
+                )
+            }!!.data
+            val majorAdapter: ArrayAdapter<String> = ArrayAdapter(
+                applicationContext,
+                android.R.layout.simple_dropdown_item_1line,
+                majors
+            )
+            majorACTV.setAdapter(majorAdapter)
 
-        // Initializing the class spinner
-        val classArray = ArrayList<String>()
-        var year = Calendar.getInstance().get(Calendar.YEAR)
-        val month = Calendar.getInstance().get(Calendar.MONTH)
-        if (month >= 6) year++  // sets minimum graduation year to next year
-        for (i in 0..4) {
-            classArray.add("Class of " + (year + i))
-        }
-
-        val classArrayAdapter: ArrayAdapter<String> = ArrayAdapter(
-            this,
-            R.layout.profile_spinner_item,
-            classArray
-        )
-        classSpinner.adapter = classArrayAdapter
-        if (profile.classOf != 0) classSpinner.setSelection(profile.classOf - year)
-
-        // Initializing the major AutoCompleteTextView
-        val majors = arrayOf(
-            "Communication", "Cognitive Science", "Computer Science"
-        )
-        val majorAdapter: ArrayAdapter<String> = ArrayAdapter(
-            this,
-            android.R.layout.simple_dropdown_item_1line,
-            majors
-        )
-        majorACTV.setAdapter(majorAdapter)
-
-        // Initializing the pronoun spinner
-        val pronoun = arrayOf(
-            "He/Him/His", "She/Her/Hers", "They/Them/Theirs"
-        )
-        val pronounAdapter: ArrayAdapter<String> = ArrayAdapter(
-            this,
-            R.layout.profile_spinner_item,
-            pronoun
-        )
-        pronounSpinner.adapter = pronounAdapter
-        when (profile.pronoun) {
-            "He" -> pronounSpinner.setSelection(0)
-            "She" -> pronounSpinner.setSelection(1)
-            "They" -> pronounSpinner.setSelection(2)
-            else -> pronounSpinner.setSelection(0)
+            // Initializing the pronoun spinner
+            val pronoun = arrayOf(
+                "He/Him/His", "She/Her/Hers", "They/Them/Theirs"
+            )
+            val pronounAdapter: ArrayAdapter<String> = ArrayAdapter(
+                applicationContext,
+                R.layout.profile_spinner_item,
+                pronoun
+            )
+            pronounSpinner.adapter = pronounAdapter
+            pronounSpinner.setSelection(
+                when (user.pronouns) {
+                    pronoun[0] -> 0
+                    pronoun[1] -> 1
+                    pronoun[2] -> 2
+                    else -> 0
+                }
+            )
         }
 
         // monitor changes in major editText and enable button if both major and hometown != empty
@@ -91,20 +118,18 @@ class CreateProfileActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable) {
             }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
                 majorFilled = s.toString().trim().isNotEmpty()
-                if (majorFilled && hometownFilled) {
-                    nextButton.isEnabled = true
-                    nextButton.isClickable = true
-                } else {
-                    nextButton.isEnabled = false
-                    nextButton.isClickable = false
-                }
+                toggleNextButton()
             }
         })
 
@@ -113,35 +138,39 @@ class CreateProfileActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable) {
             }
 
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
             }
 
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
                 hometownFilled = s.toString().trim().isNotEmpty()
-                if (majorFilled && hometownFilled) {
-                    nextButton.isEnabled = true
-                    nextButton.isClickable = true
-                } else {
-                    nextButton.isEnabled = false
-                    nextButton.isClickable = false
-                }
+                toggleNextButton()
             }
         })
 
-        nextButton.setOnClickListener{
-            // update profile in internal storage
-            profile.classOf = classSpinner.selectedItemPosition + year
-            profile.major = majorACTV.text.toString()
-            profile.hometown = hometownEditText.text.toString()
-            profile.pronoun = pronounSpinner.selectedItem.toString()
-            InternalStorage.writeObject(this, "profile", profile as Object)
+        nextButton.setOnClickListener {
+            // TODO: Update profile in backend
 
             val intent = Intent(this, ClubInterestActivity::class.java)
             startActivity(intent)
             intent.putExtra("page", 1)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+    }
+
+    // checks whether the major and hometown fields are completed, enabling or disabling the next button accordingly
+    private fun toggleNextButton() {
+        if (majorFilled && hometownFilled) {
+            nextButton.isEnabled = true
+            nextButton.isClickable = true
+        } else {
+            nextButton.isEnabled = false
+            nextButton.isClickable = false
         }
     }
 }
