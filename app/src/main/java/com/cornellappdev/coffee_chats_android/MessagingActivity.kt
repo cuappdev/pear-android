2e1f4536b11260c57a88f5ea0ff33c3ccc0018ea
Shiyuan-Huang-23 <shiyuancn@live.cn>
2021-11-20 20:52:49 -0500

    Create messaging activity displaying a chat fragment

---
+++ app/src/main/java/com/cornellappdev/coffee_chats_android/MessagingActivity.kt
@@ -0,0 +1,46 @@
+package com.cornellappdev.coffee_chats_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.cornellappdev.coffee_chats_android.fragments.messaging.ChatFragment
+
+class MessagingActivity : AppCompatActivity() {
+
+    enum class STAGE {
+        // list of all past matches
+        MESSAGES,
+        // chat with single match
+        CHAT
+    }
+
+    private var stage = STAGE.MESSAGES
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messaging)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.fragmentContainer, ChatFragment.newInstance(intent.extras!!.getInt(USER_ID), DUMMY_PEAR_ID))
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        when (stage) {
+            STAGE.MESSAGES -> {
+                super.onBackPressed()
+            }
+            STAGE.CHAT -> {
+                // TODO - change fragment
+            }
+        }
+    }
+
+    companion object {
+        const val USER_ID = "userId"
+        private const val DUMMY_PEAR_ID = -1
+    }
+}
